5b939a0ac5a5df5a66bf7fd8e7095c6611825dab
abhijato <68499000+abhijato@users.noreply.github.com>
2021-04-04 20:25:37 +0530

    Update lichessbot.py

--- lichessbot.py
+++ lichessbot.py
@@ -170,7 +170,7 @@ def play_game(li, game_id, user_profile, config):
                         if len(weight)==0 or max(weight)<9:
                             timelim=game.state["btime"]/1000
                             timelim=timelim/60
-                            divtime=85+int(len(board.move_stack)/2)
+                            divtime=85-int(len(board.move_stack)/2)
                             timep=round(timelim/divtime*60,1)
                             if timep>10:
                                 timep=10
